ba06a546ad5005123f64cfc9df58db4abd330866
lhyjmlm <kyp1634409487@outlook.com>
2023-03-27 20:53:52 +0800

    Merge branch 'master' of https://gitee.com/lhyjmlm/mlm

---
+++ src/com/chapter8/object/Finalize_.java
@@ -0,0 +1,19 @@
+package com.chapter8.object;
+
+public class Finalize_ {
+    public static void main(String[] args) {
+
+        Car bm = new Car("宝马");
+        bm = null;
+
+    }
+}
+
+class Car {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+}
