3b8622e9003af5895f4e1093e5a99875160693f2
lhyjmlm <kyp1634409487@outlook.com>
2023-06-06 18:24:50 +0800

    Merge branch 'master' of https://github.com/lhyjmlm/JavaSE1.git

---
+++ README.md
@@ -0,0 +1,10 @@
+![img.png](img.png)
+
+```
+
+  public static void main(String[] args) {
+        //封装
+        //非构造器赋值，用set()+get()
+        Person person = new Person();
+        person.setName
+```
---
+++ src/com/chapter9/HouseRentApp.java
@@ -0,0 +1,15 @@
+package com.chapter9;
+
+import com.chapter9.view.HouseView;
+
+public class HouseRentApp {
+    public static void main(String[] args) {
+
+        new HouseView().mainMenu();
+        System.out.println("====退出来房屋出租系统===");
+
+
+    }
+
+
+}
